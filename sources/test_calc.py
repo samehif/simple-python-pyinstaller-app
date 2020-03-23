b8cb9aa25bb838322946e3dd5df250864b4a67f6
Sameh Faidi <samehif@gmail.com>
2020-03-23 08:58:04 +0200

    changed to pytest instead of unittest

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -1,48 +1,40 @@
-import unittest
+import pytest
 import calc
 
-class TestCalc(unittest.TestCase):
+def test_add_integers():
     """
-    Test the add function from the calc library
+    Test that the addition of two integers returns the correct total
     """
+    result = calc.add2(1, 2)
+    assert result == 3
 
-    def test_add_integers(self):
-        """
-        Test that the addition of two integers returns the correct total
-        """
-        result = calc.add2(1, 2)
-        self.assertEqual(result, 3)
+def test_add_floats():
+    """
+    Test that the addition of two floats returns the correct result
+    """
+    result = calc.add2('10.5', 2)
+    assert result == 12.5
 
-    def test_add_floats(self):
-        """
-        Test that the addition of two floats returns the correct result
-        """
-        result = calc.add2('10.5', 2)
-        self.assertEqual(result, 12.5)
+def test_add_strings():
+    """
+    Test the addition of two strings returns the two strings as one
+    concatenated string
+    """
+    result = calc.add2('abc', 'def')
+    assert result == 'abcdef'
 
-    def test_add_strings(self):
-        """
-        Test the addition of two strings returns the two strings as one
-        concatenated string
-        """
-        result = calc.add2('abc', 'def')
-        self.assertEqual(result, 'abcdef')
+def test_add_string_and_integer():
+    """
+    Test the addition of a string and an integer returns them as one
+    concatenated string (in which the integer is converted to a string)
+    """
+    result = calc.add2('abc', 3)
+    assert result == 'abc3'
 
-    def test_add_string_and_integer(self):
-        """
-        Test the addition of a string and an integer returns them as one
-        concatenated string (in which the integer is converted to a string)
-        """
-        result = calc.add2('abc', 3)
-        self.assertEqual(result, 'abc3')
-
-    def test_add_string_and_number(self):
-        """
-        Test the addition of a string and a float returns them as one
-        concatenated string (in which the float is converted to a string)
-        """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_string_and_number():
+    """
+    Test the addition of a string and a float returns them as one
+    concatenated string (in which the float is converted to a string)
+    """
+    result = calc.add2('abc', '5.5')
+    assert result == 'abc5.5'
